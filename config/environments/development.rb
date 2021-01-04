4f54adc3105fd1ec467ba0b5d592ad387956d0f9
Ubuntu <ubuntu@ip-172-31-4-143.ec2.internal>
2021-01-04 20:11:09 +0000

    added static pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  config.hosts << "53b6eff9a2464cf391ced05998915b18.vfs.cloud9.us-east-1.amazonaws.com"
+  
 end

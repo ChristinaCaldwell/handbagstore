78999f25d5a185b17fab879d031e0e53b6236dc6
Christina Caldwell <christinacaldwell@gmail.com>
2021-01-05 23:53:23 +0000

    added cart

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,17 +1,20 @@
 class UserController < ApplicationController
     
-def login
-session[:login] = 1
-flash[:notice] = "Admin Login sucessfull!!"
-redirect_to :controller => :items
-end
 
-def logout
-session[:login] = nil
-flash[:notice] = "You have been successfully logged out!!"
-redirect_to :controller => :items
-end
-    
-    
+ def login
+ session[:login] = 1
+ session[:cart] = nil
+ flash[:notice] = "You are logged in!!"
+ redirect_to :controller => :items
+ end
+
+ def logout
+ session[:login] = nil
+ session[:cart] = nil
+ flash[:notice] = "You have been successfully logged out!!"
+ redirect_to :controller => :items
+ end
+
+
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'cart/index'
   resources :items
   # get 'static_pages/home'
   # get 'static_pages/about'
@@ -9,6 +10,11 @@ Rails.application.routes.draw do
   
   get '/login' => 'user#login'
   get '/logout' => 'user#logout'
+  
+  
+  get '/cart', to: 'cart#index'
+  get '/cart/:id', to: 'cart#add'
+  get '/cart/clear' => 'cart#clear'
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
